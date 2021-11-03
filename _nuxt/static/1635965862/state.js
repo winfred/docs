d50f339b204560c1dfc90211fb3ef0133172535a
storyaddict <storyaddict@users.noreply.github.com>
2021-11-03 19:04:49 +0000

    deploy: d709377f9836e36aef5924413f5ed2384fbeb217

---
+++ _nuxt/static/1635965862/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635965862",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cbb9e4e2"}},_img:{"/_ipx/s_1207x440/images/website/robot-type.png":"\u002F_nuxt\u002Fimage\u002F68475c.png"}}}(null,"\u002F"));
