fdf7f475f7a139b7812e421ac45607074ea908ef
jacobgkau <jacobgkau@users.noreply.github.com>
2021-11-09 22:27:52 +0000

    deploy: 889eb191fd1576be10f957371fd70194a7e635da

--- _nuxt/static/1636479741/articles/pop-shell/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636479741",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fpop-shell",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c4c96a18"}}}}(null));
