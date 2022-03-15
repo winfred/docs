05afda454aff7d6fb31ecaf1e2091e13fd4cb182
ahoneybun <ahoneybun@users.noreply.github.com>
2022-03-15 20:47:54 +0000

    deploy: b52a583493851da951c8d1cc38211d2aff1bed43

--- _nuxt/static/1647373130/articles/web-design-david-trounce-mallee/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647373130",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fweb-design-david-trounce-mallee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f5faa61"}}}}(null));
