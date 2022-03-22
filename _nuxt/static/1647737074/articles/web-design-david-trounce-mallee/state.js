f702e7dacd45cec58a2d0f5c9701a685efba99b8
gangwerz <gangwerz@users.noreply.github.com>
2022-03-22 15:11:55 +0000

    deploy: 367397f58ca7b0d98b966872cae4e35b6b899f21

--- _nuxt/static/1647737074/articles/web-design-david-trounce-mallee/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647737074",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fweb-design-david-trounce-mallee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d5cf4318"}}}}(null));
