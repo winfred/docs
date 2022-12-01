5d07d03265bd70df2834cb0aa848ba1218ae374d
ahoneybun <ahoneybun@users.noreply.github.com>
2022-12-01 20:27:29 +0000

    deploy: 2ff91520ea93364fdbad7eab816cdccc78db8964

--- _nuxt/static/1669048420/articles/touchpad-disable/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669048420",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Ftouchpad-disable",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"36acda1f"}}}}(null));
