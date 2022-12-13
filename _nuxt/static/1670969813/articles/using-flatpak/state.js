d4a08210c480acde7a1879a4bedf07c4e3f1bd65
Socialhappiness <Socialhappiness@users.noreply.github.com>
2022-12-13 23:19:19 +0000

    deploy: a99000f67714ec96686ebc628e627d4551297371

--- _nuxt/static/1670969813/articles/using-flatpak/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670969813",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fusing-flatpak",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a891856e"}}}}(null));
